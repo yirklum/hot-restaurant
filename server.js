286ed09ddcca53fb86081283a6f753cfe1aa9977
mecaputy <martina.e.caputy@gmail.com>
2018-06-08 13:52:48 -0400

    routing

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 //create array variables that will hold the data
-var personData = [{
-    name:  "", 
-    phone: "",
-    email: "",
-    uniqueId: ""
+var persondata = [{
+    name:  "martina", 
+    phone: "555-5555",
+    email: "test@gmail.com",
+    uniqueId: "12345"
 }, {
     name:  "", 
     phone: "",
@@ -23,16 +23,20 @@ var personData = [{
 
 //create a set of routes for getting and posting table data
 
-app.get("/", function(req, res){
+app.get("/index", function(req, res){
     res.sendFile(path.join(__dirname, "./index.html"));
 });
 
 app.get("/tables", function(req, res){
     res.sendFile(path.join(__dirname, "./tables.html"));
-})
+});
 
-app.get("./reservations.html", function(req, res){
+app.get("/reservations", function(req, res){
     res.sendFile(path.join(__dirname, "./reservations.html"));
+});
+
+app.get("/api/tables", function(req, res){
+    return res.json(persondata);
 })
 //create a set of routes for displaying the HTML pages
 
@@ -40,7 +44,7 @@ app.get("./reservations.html", function(req, res){
 app.post("/tables", function(req, res){
     var newperson = req.body;
     console.log(newperson);
-    personData.push(newperson);
+    persondata.push(newperson);
     res.json(newperson);
 });
 //use jQuert to run AJAz calls to GET and POST data from users to the Express server
